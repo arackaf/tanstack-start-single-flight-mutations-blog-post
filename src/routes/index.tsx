9b59b6979688a648a2f81fdb3f01f44ed4186d4a
Adam Rackis <adamrackis@hotmail.com>
2025-07-20 15:09:30 -0500

    second version

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,55 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { createMiddleware, createServerFn } from "@tanstack/react-start";
 
+//--------------------------------
+
+const queryMutationRequestMiddleware = createMiddleware({ type: "function" })
+  .client(async ({ next }) => {
+    return await next({
+      sendContext: {
+        queriesToRevalidate: { queryA: true, queryB: true },
+      },
+    });
+  })
+  .server(async ({ next, context }) => {
+    // read which queries to revalidate, that were sent from the client
+    console.log("Queries to revalidate", context.queriesToRevalidate);
+
+    const updatedQueries: Record<string, any> = {};
+
+    const value = await next({
+      sendContext: {
+        updatedQueries,
+      },
+    });
+
+    // FETCH NEW QUERY DATA
+    value.sendContext.updatedQueries.queryA = { data: "hello 1" };
+    value.sendContext.updatedQueries.queryB = { data: "hello 2" };
+
+    return value;
+  });
+
+const queryMutationResponseMiddleware = createMiddleware({ type: "function" })
+  .middleware([queryMutationRequestMiddleware])
+  .client(async ({ next }) => {
+    const result = await next();
+
+    const updatedQueries = result.context.updatedQueries;
+
+    console.log({ updatedQueries });
+
+    return result;
+  });
+
+export const callServerFn2 = createServerFn()
+  .middleware([queryMutationResponseMiddleware])
+  .handler(async () => {
+    return { value: new Date().toISOString() };
+  });
+
+//--------------------------------
+
 const queryMutationMiddleware = createMiddleware({ type: "function" })
   .client(async ({ next }) => {
     const result = await next({
