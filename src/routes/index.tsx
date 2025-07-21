1004f55a273d6cb52e89d4ca1c6d7e2649074601
Adam Rackis <adamrackis@hotmail.com>
2025-07-20 20:55:23 -0500

    try to move middleware into its own file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,119 +1,74 @@
+import { callServerFn2 } from "@/util/query-middleware";
 import { createFileRoute } from "@tanstack/react-router";
-import { createMiddleware, createServerFn } from "@tanstack/react-start";
 
 //--------------------------------
 
-const queryMutationRequestMiddleware = createMiddleware({ type: "function" })
-  .client(async ({ next }) => {
-    return await next({
-      sendContext: {
-        queriesToRevalidate: { queryA: true, queryB: true },
-      },
-    });
-  })
-  .server(async ({ next, context }) => {
-    // read which queries to revalidate, that were sent from the client
-    console.log("Queries to revalidate 2", context.queriesToRevalidate);
-
-    const updatedQueries: Record<string, any> = {};
-
-    const value = await next({
-      sendContext: {
-        updatedQueries,
-      },
-    });
-
-    // FETCH NEW QUERY DATA
-    value.sendContext.updatedQueries.queryA = { data: "hello 1" };
-    value.sendContext.updatedQueries.queryB = { data: "hello 2" };
-
-    return value;
-  });
-
-const queryMutationResponseMiddleware = createMiddleware({ type: "function" })
-  .middleware([queryMutationRequestMiddleware])
-  .client(async ({ next }) => {
-    const result = await next();
-
-    const updatedQueries = result.context.updatedQueries;
-
-    console.log({ updatedQueries });
-
-    return result;
-  });
-
-export const callServerFn2 = createServerFn()
-  .middleware([queryMutationResponseMiddleware])
-  .handler(async () => {
-    return { value: new Date().toISOString() };
-  });
-
 //--------------------------------
 
-const queryMutationMiddleware = createMiddleware({ type: "function" })
-  .client(async ({ next }) => {
-    const result = await next({
-      // this part is weird - it works without this, but the typings break. The server sets this,
-      // and again I can remove this line and it still works, but the types break. Am I missing something?
-      context: { updatedQueries: {} as Record<string, boolean> },
-      sendContext: {
-        // actual message we send to the server, telling it which queries to revalidate
-        queriesToRevalidate: { queryA: true, queryB: true } as Record<string, any>,
-      },
-    });
+// const queryMutationMiddleware = createMiddleware({ type: "function" })
+//   .client(async ({ next }) => {
+//     const result = await next({
+//       // this part is weird - it works without this, but the typings break. The server sets this,
+//       // and again I can remove this line and it still works, but the types break. Am I missing something?
+//       context: { updatedQueries: {} as Record<string, boolean> },
+//       sendContext: {
+//         // actual message we send to the server, telling it which queries to revalidate
+//         queriesToRevalidate: { queryA: true, queryB: true } as Record<string, any>,
+//       },
+//     });
 
-    const updatedQueries = result.context.updatedQueries;
-    console.log({ updatedQueries });
+//     const updatedQueries = result.context.updatedQueries;
+//     console.log({ updatedQueries });
 
-    return result;
-  })
-  .server(async ({ next, context }) => {
-    // read which queries to revalidate, that were sent from the client
-    console.log("Queries to revalidate", context.queriesToRevalidate);
+//     return result;
+//   })
+//   .server(async ({ next, context }) => {
+//     // read which queries to revalidate, that were sent from the client
+//     console.log("Queries to revalidate", context.queriesToRevalidate);
 
-    const value = await next({
-      sendContext: {
-        // seed this with an empty object, so the typings know about it
-        updatedQueries: {} as Record<string, any>,
-      },
-    });
-    // FETCH NEW QUERY DATA
-    value.sendContext.updatedQueries.queryA = { data: "hello 1" };
-    value.sendContext.updatedQueries.queryB = { data: "hello 2" };
+//     const value = await next({
+//       sendContext: {
+//         // seed this with an empty object, so the typings know about it
+//         updatedQueries: {} as Record<string, any>,
+//       },
+//     });
+//     // FETCH NEW QUERY DATA
+//     value.sendContext.updatedQueries.queryA = { data: "hello 1" };
+//     value.sendContext.updatedQueries.queryB = { data: "hello 2" };
 
-    return value;
-  });
+//     return value;
+//   });
 
-export const callServerFn = createServerFn()
-  .middleware([queryMutationMiddleware])
-  .handler(async () => {
-    return { value: new Date().toISOString() };
-  });
+// export const callServerFn = createServerFn()
+//   .middleware([queryMutationMiddleware])
+//   .handler(async () => {
+//     return { value: new Date().toISOString() };
+//   });
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
-const serverTimer = createMiddleware({ type: "function" }).server(async ({ next }) => {
-  console.log({ next });
+// const serverTimer = createMiddleware({ type: "function" }).server(async ({ next }) => {
+//   console.log({ next });
 
-  return next({
-    sendContext: {
-      xxx: 12,
-    },
-  });
-});
+//   return next({
+//     sendContext: {
+//       xxx: 12,
+//     },
+//   });
+// });
 
-const reactQueryClientMiddlewareClient = createMiddleware({ type: "function" })
-  .middleware([serverTimer])
-  .client(async ({ next }) => {
-    const result = await next();
-    console.log({ next });
+// const reactQueryClientMiddlewareClient = createMiddleware({ type: "function" })
+//   .middleware([serverTimer])
+//   .client(async ({ next }) => {
+//     const result = await next();
+//     console.log({ next });
 
-    result.context.xxx;
+//     result.context.xxx;
 
-    return result;
-  });
+//     return result;
+//   });
 
 function App() {
   function runServerFn() {
