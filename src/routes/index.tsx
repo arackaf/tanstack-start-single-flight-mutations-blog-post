717c93972b6e020e036180629dea6a95553c7b0c
Adam Rackis <adamrackis@hotmail.com>
2025-07-21 21:42:07 -0500

    import from side file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,53 +1,53 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { createMiddleware, createServerFn } from "@tanstack/react-start";
-// import { callServerFn2 } from "@/util/query-middleware";
+import { callServerFn2 } from "@/util/query-middleware";
 
-const queryMutationRequestMiddleware = createMiddleware({ type: "function" })
-  .client(async ({ next, router }) => {
-    console.log("Router", router);
+// const queryMutationRequestMiddleware = createMiddleware({ type: "function" })
+//   .client(async ({ next, router }) => {
+//     console.log("Router", router);
 
-    return await next({
-      sendContext: {
-        queriesToRevalidate: { queryA: true, queryB: true },
-      },
-    });
-  })
-  .server(async ({ next, context }) => {
-    // read which queries to revalidate, that were sent from the client
-    console.log("Queries to revalidate 2", context.queriesToRevalidate);
+//     return await next({
+//       sendContext: {
+//         queriesToRevalidate: { queryA: true, queryB: true },
+//       },
+//     });
+//   })
+//   .server(async ({ next, context }) => {
+//     // read which queries to revalidate, that were sent from the client
+//     console.log("Queries to revalidate 2", context.queriesToRevalidate);
 
-    const updatedQueries: Record<string, any> = {};
+//     const updatedQueries: Record<string, any> = {};
 
-    const value = await next({
-      sendContext: {
-        updatedQueries,
-      },
-    });
+//     const value = await next({
+//       sendContext: {
+//         updatedQueries,
+//       },
+//     });
 
-    // FETCH NEW QUERY DATA
-    value.sendContext.updatedQueries.queryA = { data: "hello 1" };
-    value.sendContext.updatedQueries.queryB = { data: "hello 2" };
+//     // FETCH NEW QUERY DATA
+//     value.sendContext.updatedQueries.queryA = { data: "hello 1" };
+//     value.sendContext.updatedQueries.queryB = { data: "hello 2" };
 
-    return value;
-  });
+//     return value;
+//   });
 
-const queryMutationResponseMiddleware = createMiddleware({ type: "function" })
-  .middleware([queryMutationRequestMiddleware])
-  .client(async ({ next }) => {
-    const result = await next();
+// const queryMutationResponseMiddleware = createMiddleware({ type: "function" })
+//   .middleware([queryMutationRequestMiddleware])
+//   .client(async ({ next }) => {
+//     const result = await next();
 
-    const updatedQueries = result.context.updatedQueries;
+//     const updatedQueries = result.context.updatedQueries;
 
-    console.log({ updatedQueries });
+//     console.log({ updatedQueries });
 
-    return result;
-  });
+//     return result;
+//   });
 
-export const callServerFn2 = createServerFn()
-  .middleware([queryMutationResponseMiddleware])
-  .handler(async () => {
-    return { value: new Date().toISOString() };
-  });
+// export const callServerFn2 = createServerFn()
+//   .middleware([queryMutationResponseMiddleware])
+//   .handler(async () => {
+//     return { value: new Date().toISOString() };
+//   });
 
 //--------------------------------
 
