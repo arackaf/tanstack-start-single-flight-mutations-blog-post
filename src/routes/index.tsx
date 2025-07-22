ab1694330bb6368d71a3cec4adae1f1513a4ea50
Adam Rackis <adamrackis@hotmail.com>
2025-07-21 20:20:17 -0500

    working now!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,53 @@
-//import { callServerFn2 } from "@/util/query-middleware";
 import { createFileRoute } from "@tanstack/react-router";
+import { createMiddleware, createServerFn } from "@tanstack/react-start";
+// import { callServerFn2 } from "@/util/query-middleware";
+
+const queryMutationRequestMiddleware = createMiddleware({ type: "function" })
+  .client(async ({ next, router }) => {
+    console.log("Router", router);
+
+    return await next({
+      sendContext: {
+        queriesToRevalidate: { queryA: true, queryB: true },
+      },
+    });
+  })
+  .server(async ({ next, context }) => {
+    // read which queries to revalidate, that were sent from the client
+    console.log("Queries to revalidate 2", context.queriesToRevalidate);
+
+    const updatedQueries: Record<string, any> = {};
+
+    const value = await next({
+      sendContext: {
+        updatedQueries,
+      },
+    });
+
+    // FETCH NEW QUERY DATA
+    value.sendContext.updatedQueries.queryA = { data: "hello 1" };
+    value.sendContext.updatedQueries.queryB = { data: "hello 2" };
+
+    return value;
+  });
+
+const queryMutationResponseMiddleware = createMiddleware({ type: "function" })
+  .middleware([queryMutationRequestMiddleware])
+  .client(async ({ next }) => {
+    const result = await next();
+
+    const updatedQueries = result.context.updatedQueries;
+
+    console.log({ updatedQueries });
+
+    return result;
+  });
+
+export const callServerFn2 = createServerFn()
+  .middleware([queryMutationResponseMiddleware])
+  .handler(async () => {
+    return { value: new Date().toISOString() };
+  });
 
 //--------------------------------
 
@@ -72,9 +120,9 @@ export const Route = createFileRoute("/")({
 
 function App() {
   function runServerFn() {
-    //   callServerFn2().then((result) => {
-    //     console.log({ result });
-    // });
+    callServerFn2().then((result) => {
+      console.log({ result });
+    });
   }
 
   return (
