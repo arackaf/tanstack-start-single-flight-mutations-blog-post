f4fed8200b498c967d2f5b71a26bdc3c684997df
Adam Rackis <adamrackis@hotmail.com>
2025-08-10 19:58:09 -0500

    remove dead code

--- backend/fetchUtils.ts
+++
@@ -1,9 +0,0 @@
-export const postToApi = <T>(relativeUrl: string, body = {}) => {
-  return fetch(`http://localhost:3001/${relativeUrl}`, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(resp => resp.json() as T);
-};
