fabd959d108f05e67919cf3549c396a34d680352
Adam Rackis <adamrackis@hotmail.com>
2025-08-11 22:45:36 -0500

    work

--- src/middleware/refetch.ts
+++ src/middleware/refetch.ts
@@ -6,9 +6,16 @@ export const refetchMiddleware = (queryKey: QueryKey | QueryKey[]) =>
     .client(async ({ next, context, router }) => {
       const allKeys = Array.isArray(queryKey) ? queryKey : [queryKey];
       const queryClient = router.options.context.queryClient;
+      const cache = queryClient.getQueryCache();
 
       const allQueriesFound = allKeys.flatMap(key => queryClient.getQueriesData({ queryKey: key, exact: false }));
-      console.log("queriesFound", allQueriesFound);
+      //console.log("queriesFound", allQueriesFound);
+      allQueriesFound.forEach(query => {
+        const key = query[0];
+        const entry = cache.find({ queryKey: key, exact: true });
+        const isActive = !!entry?.observers?.length;
+        console.log({ key: key.join("-"), isActive });
+      });
 
       const result = await next({
         sendContext: {}
