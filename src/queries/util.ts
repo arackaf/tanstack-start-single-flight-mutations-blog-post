c4d1b2fb8c456b954db25c42c6d9663ce9e4e3e0
Adam Rackis <adamrackis@hotmail.com>
2026-01-16 16:22:54 -0600

    start updates to queryOptions helper

--- src/queries/util.ts
+++ src/queries/util.ts
@@ -1,37 +1,43 @@
 import { IntersectAllValidatorInputs, RequiredFetcher } from "@tanstack/react-start";
-import { QueryKey, queryOptions, AnyUseQueryOptions } from "@tanstack/react-query";
+import { QueryKey, queryOptions, AnyUseQueryOptions, UnusedSkipTokenOptions } from "@tanstack/react-query";
 import { Expand } from "@tanstack/react-router";
 
 type OtherQueryOptions = Omit<AnyUseQueryOptions, "queryKey" | "queryFn" | "meta">;
 
-type ServerFnArgs<TServerFn extends RequiredFetcher<any, any, any>> =
-  TServerFn extends RequiredFetcher<any, infer TArgs, any>
-    ? TArgs extends undefined
-      ? []
-      : [Expand<IntersectAllValidatorInputs<any, TArgs>>]
-    : never;
+type ServerFnArgs<TFn extends RequiredFetcher<any, any, any>> = Parameters<TFn>[0] extends infer TRootArgs
+  ? TRootArgs extends { data: infer TResult }
+    ? TResult
+    : undefined
+  : never;
+
+type ServerFnHasArgs<TFn extends RequiredFetcher<any, any, any>> =
+  ServerFnArgs<TFn> extends infer U ? (U extends undefined ? true : false) : false;
+
+// type ServerFnArgs<TServerFn extends RequiredFetcher<any, any, any>> =
+//   TServerFn extends RequiredFetcher<any, infer TArgs, any>
+//     ? TArgs extends undefined
+//       ? []
+//       : [Expand<IntersectAllValidatorInputs<any, TArgs>>]
+//     : never;
 
 export function revalidatedQueryOptions<T, U>(
   prefixKey: QueryKey,
   serverFn: RequiredFetcher<any, T, Promise<U>>,
-  args: ServerFnArgs<RequiredFetcher<any, T, Promise<U>>>,
+  arg: ServerFnArgs<RequiredFetcher<any, T, Promise<U>>>,
   otherQueryOptions?: OtherQueryOptions
-) {
+): UnusedSkipTokenOptions<U> {
   const prefixKeyArr = Array.isArray(prefixKey) ? prefixKey : [prefixKey];
 
   return queryOptions<U>({
     ...otherQueryOptions,
-    queryKey: [...prefixKeyArr, ...args],
+    queryKey: [...prefixKeyArr, arg],
     queryFn: async (): Promise<U> => {
-      if (args.length === 0) {
-        return serverFn({ data: undefined as any });
-      }
-      return serverFn({ data: args[0] });
+      return serverFn({ data: arg });
     },
     meta: {
       __revalidate: {
         serverFn,
-        args
+        arg
       }
     }
   });
--- src/serverFn/epics.ts
+++ src/serverFn/epics.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
+import { createServerFn, RequiredFetcher } from "@tanstack/react-start";
 import { db } from "@/drizzle/db";
 import { epics as epicsTable, tasks as tasksTable, milestones as milestonesTable } from "@/drizzle/schema";
 import { asc, count, desc, eq } from "drizzle-orm";
@@ -31,7 +31,7 @@ export const getEpicsCount = createServerFn({ method: "GET" }).handler(async ({}
   return { count };
 });
 
-export const getEpicsOverview = createServerFn({ method: "GET" }).handler(async ({}) => {
+export const getEpicsSummary = createServerFn({ method: "GET" }).handler(async ({}) => {
   const subQuery = db
     .select({ epicId: epicsTable.id, count: count().as("count") })
     .from(epicsTable)
@@ -61,6 +61,17 @@ export const getEpicMilestones = createServerFn({ method: "GET" })
     return milestones;
   });
 
+export const updateWithSimpleRefetch = createServerFn({ method: "POST" })
+  .inputValidator((obj: { id: number; name: string }) => obj)
+  .handler(async ({ data }) => {
+    await new Promise(resolve => setTimeout(resolve, 1000 * Math.random()));
+    await db.update(epicsTable).set({ name: data.name }).where(eq(epicsTable.id, data.id));
+
+    const [epicsList, epicsSummaryData] = await Promise.all([getEpicsList({ data: 1 }), getEpicsSummary()]);
+
+    return { epicsList, epicsSummaryData };
+  });
+
 export const updateEpic = createServerFn({ method: "POST" })
   .middleware([
     refetchMiddleware({
@@ -82,3 +93,59 @@ export const updateEpicWithRedirect = createServerFn({ method: "POST" })
 
     throw redirect({ to: "/app/epics", search: { page: 1 } });
   });
+
+export const updateEpicWithRedirect2 = createServerFn({ method: "POST" })
+  .inputValidator((obj: { id: number; name: string }) => obj)
+  .handler(async ({ data }) => {
+    await new Promise(resolve => setTimeout(resolve, 1000 * Math.random()));
+    await db.update(epicsTable).set({ name: data.name }).where(eq(epicsTable.id, data.id));
+
+    throw redirect({ to: "/app/epics", search: { page: 1 } });
+  });
+
+const noArgs = createServerFn({ method: "POST" }).handler(async () => {});
+const args = createServerFn({ method: "POST" })
+  .inputValidator((obj: string) => obj)
+  .handler(async () => {});
+
+type ServerFnArgs<TFn extends RequiredFetcher<any, any, any>> = Parameters<TFn>[0] extends infer TRootArgs
+  ? TRootArgs extends { data: infer TResult }
+    ? TResult
+    : undefined
+  : never;
+
+type ServerFnHasArgs<TFn extends RequiredFetcher<any, any, any>> =
+  ServerFnArgs<TFn> extends infer U ? (U extends undefined ? false : true) : false;
+
+type ServerFnWithArgs<TFn extends RequiredFetcher<any, any, any>> = ServerFnHasArgs<TFn> extends true ? TFn : never;
+type ServerFnWithoutArgs<TFn extends RequiredFetcher<any, any, any>> = ServerFnHasArgs<TFn> extends false ? TFn : never;
+
+type Test = ServerFnHasArgs<typeof noArgs>;
+//    ^?
+type Test2 = ServerFnHasArgs<typeof args>;
+//    ^?
+
+function foo<TFn extends RequiredFetcher<any, any, any>>(
+  _serverFn: ServerFnWithArgs<TFn>,
+  arg: ServerFnArgs<TFn>
+): number;
+function foo<TFn extends RequiredFetcher<any, any, any>>(_serverFn: ServerFnWithoutArgs<TFn>): number;
+function foo<TFn extends RequiredFetcher<any, any, any>>(
+  _serverFn: ServerFnWithoutArgs<TFn> | ServerFnWithArgs<TFn>,
+  _arg?: ServerFnArgs<TFn>
+) {
+  return 0;
+}
+
+foo(noArgs);
+
+// @ts-expect-error
+foo(noArgs, "hello");
+
+// @ts-expect-error
+foo(args);
+
+foo(args, "hello");
+
+// @ts-expect-error
+foo(args, 12);
