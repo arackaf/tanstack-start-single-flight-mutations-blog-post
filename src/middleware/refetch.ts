2c12e8ddf87b3af6d397b8e610ee94d89fa35012
Adam Rackis <adamrackis@hotmail.com>
2026-01-17 19:53:16 -0600

    split middleware

--- src/middleware/refetch.ts
+++ src/middleware/refetch.ts
@@ -92,7 +92,7 @@ type RefetchMiddlewareConfig = {
   refetch: QueryKey[];
 };
 
-export const refetchMiddleware = createMiddleware({ type: "function" })
+export const old_refetchMiddleware = createMiddleware({ type: "function" })
   .inputValidator((config?: RefetchMiddlewareConfig) => config)
   .client(async ({ next, data }) => {
     const { refetch = [] } = data ?? {};
@@ -152,6 +152,81 @@ export const refetchMiddleware = createMiddleware({ type: "function" })
         key: refetchPayload.key,
         result: await refetchPayload.result
       });
+    }
+
+    return result;
+  });
+
+
+const prelimRefetchMiddleware = createMiddleware({ type: "function" })
+  .inputValidator((config?: RefetchMiddlewareConfig) => config)
+  .client(async ({ next, data }) => {
+    const { refetch = [] } = data ?? {};
+
+    const router = await getRouterInstance();
+    const queryClient: QueryClient = router.options.context.queryClient;
+    const cache = queryClient.getQueryCache();
+
+    const revalidate: RevalidationPayload = {
+      refetch: []
+    };
+
+    refetch.forEach((key: QueryKey) => {
+      const entry = cache.find({ queryKey: key, exact: true });
+      if (!entry) return;
+
+      const revalidatePayload: any = entry?.options?.meta?.__revalidate ?? null;
+
+      if (revalidatePayload) {
+        revalidate.refetch.push({
+          key,
+          fn: revalidatePayload.serverFn,
+          arg: revalidatePayload.arg
+        });
+      }
+    });
+
+    return await next({
+      sendContext: {
+        revalidate
+      }
+    });
+  })
+  .server(async ({ next, context }) => {
+    const result = await next({
+      sendContext: {
+        payloads: [] as any[]
+      }
+    });
+
+    const allPayloads = context.revalidate.refetch.map(refetchPayload => {
+      return {
+        key: refetchPayload.key,
+        result: refetchPayload.fn({ data: refetchPayload.arg })
+      };
+    });
+
+    for (const refetchPayload of allPayloads) {
+      result.sendContext.payloads.push({
+        key: refetchPayload.key,
+        result: await refetchPayload.result
+      });
+    }
+
+    return result;
+  });
+
+export const refetchMiddleware = createMiddleware({ type: "function" })
+  .middleware([prelimRefetchMiddleware])
+  .inputValidator((config?: RefetchMiddlewareConfig) => config)
+  .client(async ({ next }) => {
+    const router = await getRouterInstance();
+    const queryClient: QueryClient = router.options.context.queryClient;
+
+    const result = await next();
+
+    for (const entry of result.context?.payloads ?? []) {
+      queryClient.setQueryData(entry.key, entry.result, { updatedAt: Date.now() });
     }
 
     return result;
